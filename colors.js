3cf7e0ca2d9b1de6daf2749717fb9796d4d1e01c
unknown <leeme1824@kaist.ac.kr>
2020-10-20 19:21:59 +0900

    spliting by files!!! done!!!

---
+++ colors.js
@@ -0,0 +1,47 @@
+
+  var Links ={
+    links : document.querySelectorAll('a'),//해결못함, 밑에서 변수 안만들고 이렇게 하고 싶은데...
+    setColor : function(color){
+      var links = document.querySelectorAll('a');
+      for(let i=0;i<links.length;i++){
+        links[i].style.color=color;
+      }
+
+
+      /*for(let i =0 ;i<links.length;i++){
+        document.write(links[i]+"sss"+"<br>")
+      }*/
+    }
+
+  }
+
+  function activeSetColor(color){
+    var active = document.querySelector('#active');
+    active.style.color = color;
+  }
+
+  var Body={
+    setColor : function (color){
+      document.querySelector('body').style.color =color;
+    },
+    setBGColor : function (color){
+      document.querySelector('body').style.backgroundColor =color;
+    }
+  }
+
+  function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value ==='night'){
+      Body.setBGColor('black');
+      Body.setColor('white');
+      self.value='day';
+      Links.setColor('white');
+      activeSetColor('lightcoral');
+    } else{
+      Body.setBGColor('white');
+      Body.setColor('black');
+      self.value='night';
+      Links.setColor('black');
+      activeSetColor('red');
+    }
+  }
